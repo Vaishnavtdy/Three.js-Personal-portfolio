0731c01bb5f5e46177826c4a7bebe02fffa6766f
Vaishnav Vijayan <vaishnavtdy@gmail.com>
2023-03-31 10:34:20 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,8 @@ const Navbar = () => {
           setActive("")
           window.scrollTo(0, 0)
         }}>
-          <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Vaishnav</p>
+          <img src={logo} alt="logo" className='w-12 h-12 object-contain' />
+          {/* <p className='text-white text-[18px] font-bold cursor-pointer'>Vaishnav</p> */}
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks?.map((link) => (
